f2cfd8294b0d33e6986d6f68cf035761c72ace95
riozo <riozo@protonmail.com>
2019-10-06 19:33:52 +0330

    q

--- pardakht/gateways/parsian.py
+++ pardakht/gateways/parsian.py
@@ -57,7 +57,7 @@ def get_token(request: HttpRequest, payment):
 
 
 def verify(request, payment):
-    logger.debug(request.POST.get('status') != 0)
+    logger.debug(request.POST.get('status'))
     if request.POST.get('status') != 0:
         logger.debug("status no 0")
         payment.state = payment.STATE_FAILURE
